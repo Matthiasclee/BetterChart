ceb1c753ea4b37238f030ec5a88181636da4ee3f
Matthias Lee <matthiasclee09@gmail.com>
2021-08-27 07:04:30 -0700

    Update btcchart.rb

--- btcchart.rb
+++ btcchart.rb
@@ -3,12 +3,16 @@ require 'httparty'
 require './chart.rb'
 system("clear")
 now = Time.now.to_s.split(" ")[0]
-timestamp = ARGV[0]
-olddate = Time.at(Time.now.to_i - (86400 * (timestamp.to_i - 1))).to_s.split(" ")[0]
-olddate = "2010-07-17"
+if ARGV[0] == nil
+	olddate = "2010-07-17"
+else
+	timestamp = ARGV[0]
+	olddate = Time.at(Time.now.to_i - (86400 * (timestamp.to_i - 1))).to_s.split(" ")[0]
+end
+
 data = JSON.parse(HTTParty.get("https://api.coindesk.com/v1/bpi/historical/close.json?start=#{olddate}&end=#{now}"))["bpi"]
 prices = data.values
-puts chart(prices, "$", "=")
+puts chart(prices, "$", "*")
 
 
 
